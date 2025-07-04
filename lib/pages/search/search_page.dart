4ffd0df8fe45ef998880aa99b2fff6199bb0fddb
HoTrungY <hotrungy2242004@gmail.com>
2025-07-04 22:40:05 +0700

    Add full Flutter project

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -1,97 +1,97 @@
 
-// import 'package:edu_share/components/app_bar/app_tab_bar.dart';
-// import 'package:edu_share/components/app_search_box.dart';
-// import 'package:edu_share/pages/course_detail/course_detail_page.dart';
-// import 'package:edu_share/pages/profile/profile_page.dart';
-// import 'package:edu_share/pages/search/search_vm.dart';
-// import 'package:edu_share/pages/search/widgets/create_course_item.dart';
-// import 'package:edu_share/resource/img/app_images.dart';
-// import 'package:edu_share/resource/themes/app_colors.dart';
-// import 'package:edu_share/resource/themes/app_style.dart';
-// import 'package:edu_share/service/local/shared_prefs.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:stacked/stacked.dart';
+import 'package:edu_share/components/app_bar/app_tab_bar.dart';
+import 'package:edu_share/components/app_search_box.dart';
+import 'package:edu_share/pages/course_detail/course_detail_page.dart';
+import 'package:edu_share/pages/profile/profile_page.dart';
+import 'package:edu_share/pages/search/search_vm.dart';
+import 'package:edu_share/pages/search/widgets/create_course_item.dart';
+import 'package:edu_share/resource/img/app_images.dart';
+import 'package:edu_share/resource/themes/app_colors.dart';
+import 'package:edu_share/resource/themes/app_style.dart';
+import 'package:edu_share/service/local/shared_prefs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stacked/stacked.dart';
 
-// class SearchPage extends StackedView<SearchVM> {
-//   const SearchPage({super.key});
+class SearchPage extends StackedView<SearchVM> {
+  const SearchPage({super.key});
 
-//   @override
-//   void onViewModelReady(SearchVM viewModel) {
-//     super.onViewModelReady(viewModel);
-//     viewModel.onInit();
-//   }
+  @override
+  void onViewModelReady(SearchVM viewModel) {
+    super.onViewModelReady(viewModel);
+    viewModel.onInit();
+  }
 
-//   @override
-//   SearchVM viewModelBuilder(BuildContext context) => SearchVM();
+  @override
+  SearchVM viewModelBuilder(BuildContext context) => SearchVM();
 
-//   @override
-//   Widget builder(BuildContext context, SearchVM viewModel, Widget? child) {
-//     return Scaffold(
-//       backgroundColor: AppColor.bgColor,
-//       appBar: AppTabBar(
-//         rightPressed: () => Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => const ProfilePage()),
-//         ),
-//         title: 'What do you want to learn today?',
-//         avatar: SharedPrefs.user?.avatar ?? '',
-//       ),
-//       body: ListView(
-//         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//         children: [
-//           SizedBox(
-//             height: 51.0,
-//             child: AppSearchBox(
-//               onChanged: (value) {
-//                 viewModel.searchCourse(value);
-//                 SharedPrefs.setSearchText(value);
-//               },
-//               searchController: viewModel.searchController,
-//               searchFocus: viewModel.searchFocus,
-//             ),
-//           ),
-//           viewModel.searchList.isEmpty
-//               ? Column(
-//                   children: [
-//                     const SizedBox(height: 160.0),
-//                     GestureDetector(
-//                         onTap: () => viewModel.searchFocus.requestFocus(),
-//                         child: SvgPicture.asset(
-//                           AppImages.iconSearch,
-//                           color: AppColor.blue,
-//                           fit: BoxFit.cover,
-//                         )),
-//                     const SizedBox(height: 6.0),
-//                     Text(
-//                       'Search The Course',
-//                       style: AppStyles.STYLE_18.copyWith(
-//                         color: AppColor.textColor,
-//                       ),
-//                     ),
-//                   ],
-//                 )
-//               : ListView.separated(
-//                   physics: const NeverScrollableScrollPhysics(),
-//                   shrinkWrap: true,
-//                   itemCount: viewModel.searchList.length,
-//                   separatorBuilder: (_, __) => const SizedBox(),
-//                   itemBuilder: (context, idx) {
-//                     final course = viewModel.searchList[idx];
-//                     return CourseSearchItem(
-//                       course,
-//                       onTap: () => Navigator.of(context).push(
-//                         MaterialPageRoute(
-//                           builder: (context) => CourseDetailPage(
-//                             course.docId ?? "",
-//                           ),
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                 )
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget builder(BuildContext context, SearchVM viewModel, Widget? child) {
+    return Scaffold(
+      backgroundColor: AppColor.bgColor,
+      appBar: AppTabBar(
+        rightPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const ProfilePage()),
+        ),
+        title: 'What do you want to learn today?',
+        avatar: SharedPrefs.user?.avatar ?? '',
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        children: [
+          SizedBox(
+            height: 51.0,
+            child: AppSearchBox(
+              onChanged: (value) {
+                viewModel.searchCourse(value);
+                SharedPrefs.setSearchText(value);
+              },
+              searchController: viewModel.searchController,
+              searchFocus: viewModel.searchFocus,
+            ),
+          ),
+          viewModel.searchList.isEmpty
+              ? Column(
+                  children: [
+                    const SizedBox(height: 160.0),
+                    GestureDetector(
+                        onTap: () => viewModel.searchFocus.requestFocus(),
+                        child: SvgPicture.asset(
+                          AppImages.iconSearch,
+                          color: AppColor.blue,
+                          fit: BoxFit.cover,
+                        )),
+                    const SizedBox(height: 6.0),
+                    Text(
+                      'Search The Course',
+                      style: AppStyles.STYLE_18.copyWith(
+                        color: AppColor.textColor,
+                      ),
+                    ),
+                  ],
+                )
+              : ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: viewModel.searchList.length,
+                  separatorBuilder: (_, __) => const SizedBox(),
+                  itemBuilder: (context, idx) {
+                    final course = viewModel.searchList[idx];
+                    return CourseSearchItem(
+                      course,
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CourseDetailPage(
+                            course.docId ?? "",
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )
+        ],
+      ),
+    );
+  }
+}
 
